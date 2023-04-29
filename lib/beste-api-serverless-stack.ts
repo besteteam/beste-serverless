41cfb80ee6f0bda663181ca0926bca5322c4adcf
bulingen <adamlecorney@gmail.com>
2023-04-29 21:58:16 +0200

    feat: cdk init

---
+++ lib/beste-api-serverless-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class BesteApiServerlessStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'BesteApiServerlessQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
