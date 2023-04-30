d5ec54b5ebf35ae007f3f84be3cbb35849658799
bulingen <adamlecorney@gmail.com>
2023-04-30 15:39:28 +0200

    feat: cdk init

---
+++ test/beste-next-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BesteNextServerless from '../lib/beste-next-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/beste-next-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BesteNextServerless.BesteNextServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
